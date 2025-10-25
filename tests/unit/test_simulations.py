5e36458a00e47d14ec8265c1849b67a419a01a5e
HenryJordan500 <henry.jordan@colorado.edu>
2025-10-25 14:44:30 -0600

    Added outlines for tests

---
+++ tests/unit/test_simulations.py
@@ -0,0 +1,17 @@
+import numpy as np
+import h5py
+import unittest
+
+
+# Test simulation flow object works
+# Test simulation space works
+# Test simulation parameters work
+# Test Particle Initialzation
+
+# Test material derivative
+# Test diff_eq
+# Test RK4 step
+# Test particle array setup works
+# Test time step arrary slicing works
+# Test packaging to acc,vos,pos
+# Test Saving to h5py works
