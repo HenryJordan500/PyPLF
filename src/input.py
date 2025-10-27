745f0d4ce315ed024b49a9a88bd6271bf876de86
Tayler Martin <tayler.martin@colorado.edu>
2025-10-27 15:28:52 -0600

    Starting point for initialization and validation

--- src/input.py
+++ src/input.py
@@ -1,1 +1,3 @@
 # This file takes input from user either in analytical form or as a numerical (.csv) file
+import h5py
+
--- src/validation.py
+++ src/validation.py
@@ -1,2 +1,25 @@
+import numpy as np
+import matplotlib as mpl
+import h5py
+
 def linear_flow(x):
     return 1
+
+def validate_particle_flow(position_array,velocity_array,acceleration_array):
+    # Calculate average position, velocity, and acceleration
+    avgposition = []
+    avgvelocity = []
+    avgacceleration = []
+    for i in range(position_array[1,:]):
+        avgposition[i] = np.mean(position_array[:,i])
+    for i in range(velocity_array[1,:]):
+        avgvelocity[i] = np.mean(velocity_array[:,i])
+    for i in range(acceleration_array[1,:]):
+        avgacceleration[i] = np.mean(acceleration_array[:,i])
+    
+    
+    return 1
+
+def get_data(file_name):
+    hf = h5py.File(file_name, 'r')
+    hf.keys()
