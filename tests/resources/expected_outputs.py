90bd403105fa3507f605540ce212d566f6764580
HenryJordan500 <henry.jordan@colorado.edu>
2025-11-27 21:31:16 -0500

    Added resources folder with expected outputs for testing

---
+++ tests/resources/expected_outputs.py
@@ -0,0 +1,19 @@
+
+
+
+### Initalization Testing ###
+
+def intialization_flow(x, t):
+
+    return x + t
+        
+def intialization_spatial_derivative(x, t):
+
+    return 1 + t
+
+def intialization_time_derivative(x, t):
+
+    return x + 1
+
+
+### Simulation Testing ###
